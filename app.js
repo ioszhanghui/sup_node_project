048cb2d5050f78d32afbf66f3a987a0d843b9708
dorayo <yomingyo@gmail.com>
2018-06-05 22:13:55 +0800

    提交

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var express =require('express');
 var mysql = require('mysql');
 var querystring = require('querystring');
 var app = express();
-
 var bodyParser = require('body-parser');
-app.use(body_parser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var jsonParser = bodyParser.json();//application/json解析
 
 var pool = mysql.createPool({"host":"39.105.115.133","user":"root","password":"123456","database":"mysql","port":"3306"})
 /*跨域问题*/
@@ -14,8 +12,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 /*获取活动列表*/
-app.get('/discountList',function(req,res,next){
+app.post('/discountList',jsonParser,function(req,res,next){
 	console.log(req.body.cust_no);
 })
 app.listen(8081,function () {
