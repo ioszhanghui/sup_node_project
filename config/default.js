c51a891c0523cfd544a297eaf89ce7cd9882b9a5
dorayo <yomingyo@gmail.com>
2018-06-22 07:44:02 +0800

    提交

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 /*全局的配置*/
 /*数据库的链接配置*/
 var mysqlConfig ={
-	"host":"121.43.171.57",
-	"user":"appxhb",
-	"password":"120120",
-	"database":"appxhb",
+	"host":"www.yidiankeji.online",
+	"user":"root",
+	"password":"123456",
+	"database":"mysql",
 	"port":"3306",
 	"connectionLimit":"20",//连接池可创建的最大链接数
 	"waitForConnections":true,
